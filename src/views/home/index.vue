6ed8e0ad454212c56aa53000beddde37226062bd
topninja <lyu@fivestarsmobi.com>
2021-09-24 01:26:56 +0900

    store test without vuex

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
     <img class="m-auto max-w-xs" alt="logo" :src="synthsLogo" />
-    <SynthsNew class="mt-5" msg="Welcome to Yam Synths!" />
+    <SynthsNew class="mt-5" :msg="'Welcome ' + userDetails.name " />
   </div>
 </template>
 
 <script lang="ts">
-import SynthsNew from "./components/SynthsNew.vue"
+import SynthsNew from "../../components/SynthsNew.vue"
 import synthsLogo from "../../assets/images/logo.png"
-
-export default {
+import { defineComponent, inject, computed } from "vue";
+export default defineComponent({
   name: "Home",
   data: function () {
     return {
@@ -19,5 +19,11 @@ export default {
   components: {
     SynthsNew,
   },
-}
+  setup() {
+    const userDetails = inject("userDetails");
+    return {
+      userDetails
+    };
+  }
+})
 </script>
