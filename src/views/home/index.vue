a1e50eebb1bc27d1d9ac8e1b7345de596a5f5b94
Paul <mmg.lehkyi.p@gmail.com>
2021-09-23 17:19:31 +0300

    Tailwind Settings

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,29 +1,10 @@
 <template>
-  <div class="home">
-    <img class="m-auto max-w-xs" alt="logo" :src="synthsLogo" />
-    <SynthsNew class="mt-5" :msg="'Welcome ' + userDetails.name " />
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
-import SynthsNew from "../../components/SynthsNew.vue"
-import synthsLogo from "../../assets/images/logo.png"
-import { defineComponent, inject, computed } from "vue";
+import { defineComponent } from "vue";
 export default defineComponent({
   name: "Home",
-  data: function () {
-    return {
-      synthsLogo: synthsLogo,
-    }
-  },
-  components: {
-    SynthsNew,
-  },
-  setup() {
-    const userDetails: any = inject("userDetails");
-    return {
-      userDetails
-    };
-  }
 })
 </script>
