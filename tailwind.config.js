5cc68f5e88c2c97a6edbf10f1bf9911266d44091
Paul <mmg.lehkyi.p@gmail.com>
2021-09-23 17:01:01 +0300

    Tailwind Init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  darkMode: false,
   theme: {
-    extend: {},
+    fontFamily: {
+      openSauceSans: ['Open Sauce Sans','sans-serif'],
+    },
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      blackLight: '#11112F',
+      blackDark: '#0C0C28',
+      purpleLight: '#7171B2',
+      purpleDark: '#525284',
+      blueDark: '#212149',
+
+    },
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
   },
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   variants: {
     extend: {},
   },
