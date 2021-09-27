aaea35e9674e1666bb5de88ea9b44480ec57e90a
ethedev <2432272+ethedev@users.noreply.github.com>
2021-09-27 20:31:24 +0400

    update build

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/explore">Explore</router-link>
+    <router-link to="/insidebar">Explore</router-link>
+  </div>
   <router-view />
 </template>
+
+<style lang="scss">
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
 
 <script lang="ts">
 import { defineComponent, provide, reactive, ref, onUnmounted } from "vue";
