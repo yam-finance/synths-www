aab8bfb86eb3c322717432cd0e161bfe93ad8c04
ethedev <2432272+ethedev@users.noreply.github.com>
2021-10-01 20:04:27 +0400

    fix too many conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     extend: {},
     placeholderColor: {
       'primary': '#525284',
-    }
+    },
     fontFamily: {
       openSauceSans: ['Open Sauce Sans','sans-serif'],
     },
@@ -20,7 +20,6 @@ module.exports = {
       purpleLight: '#7171B2',
       purpleDark: '#525284',
       blueDark: '#212149',
-
     },
     screens: {
       'xs': '475px',
