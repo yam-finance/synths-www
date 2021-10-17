a3a0f073efacb395f94fdc04a2e7f436e78e6114
jason <kwon_jason@yahoo.com>
2021-10-18 02:30:13 +1000

    fix build issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
         '@typescript-eslint/no-undef': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
-        'vue/script-setup-uses-vars': 'error'
+        'vue/script-setup-uses-vars': 'error',
+        'no-console': 'off',
     },
     globals: {
         $ref: 'readonly',
