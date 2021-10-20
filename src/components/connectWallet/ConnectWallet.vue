184e51ed3335729bbea60ea0fb5910b175366d51
jason <kwon_jason@yahoo.com>
2021-10-20 15:03:23 +1000

    update font, style,popup position, tailwind

--- src/components/connectWallet/ConnectWallet.vue
+++ src/components/connectWallet/ConnectWallet.vue
@@ -1,275 +1,331 @@
 <template>
-  <div class="modal-backdrop">
-    <div class="modal">
-      <img class="wallet_gif" src="../../assets/icons/Wallet.png" />
+  <div
+    @click="$emit('close')"
+    class="modal-backdrop flex items-center justify-center fixed inset-0"
+  >
+    <div class="modal rounded-lg flex flex-col items-start h-56 p-4 absolute">
+      <img class="wallet_gif h-32 absolute w-40" src="@/assets/icons/Wallet.png" />
 
-      <div class="title">
-        <div class="header connect_wallet_header">Connect your wallet</div>
-        <div class="connect_wallet_info">Start using Yarn Synths with your favourite wallet</div>
+      <div
+        class="
+          title
+          justify-between
+          font-semibold
+          text-base
+          leading-5
+          relative
+          text-white
+          not-italic
+        "
+      >
+        <div
+          class="
+            header
+            connect_wallet_header
+            flex-none
+            order-none
+            font-semibold
+            text-base
+            leading-5
+            mx-0
+            my-2
+            text-white
+            not-italic
+          "
+        >
+          Connect your wallet
+        </div>
+        <div class="flex-none order-1 h-10 text-sm mx-0 my-2 font-normal static not-italic w-56">
+          Start using Yarn Synths with your favourite wallet
+        </div>
       </div>
 
-      <div class="connect_wallet_container">
-        <div @click="$emit('connect', 'injected')" class="wallet_box">
-          <img src="../../assets/icons/metamask.svg" />
-          <div class="wallet_name" >Metamask</div>
+      <div
+        class="
+          connect_wallet_container
+          rounded-2xl
+          flex flex-wrap
+          items-start
+          h-24
+          px-2
+          py-3
+          absolute
+        "
+      >
+        <div
+          @click="$emit('connect', 'injected')"
+          class="
+            wallet_box
+            rounded-xl
+            cursor-pointer
+            flex flex-row
+            items-center
+            justify-start
+            flex-none
+            order-none
+            h-8
+            text-xs
+            my-0
+            mx-2
+            py-2
+            pl-2
+            pr-4
+            static
+            top-0
+            left-0
+          "
+        >
+          <img src="@/assets/icons/metamask.svg" />
+          <div
+            class="
+              cursor-pointer
+              flex-none
+              order-1
+              h-4
+              text-xs
+              leading-4
+              my-0
+              mx-2
+              static
+              text-white
+              not-italic
+              wallet_name
+            "
+          >
+            Metamask
+          </div>
         </div>
-        <div @click="$emit('connect', 'walletconnect')" class="wallet_box">
-          <img src="../../assets/icons/walletconnect.png" />
-          <div class="wallet_name" >WalletConnect &nbsp;</div>
+        <div
+          @click="$emit('connect', 'walletconnect')"
+          class="
+            wallet_box
+            rounded-xl
+            cursor-pointer
+            flex flex-row
+            items-center
+            justify-start
+            flex-none
+            order-none
+            h-8
+            text-xs
+            my-0
+            mx-2
+            py-2
+            pl-2
+            pr-4
+            static
+            top-0
+            left-0
+          "
+        >
+          <img src="@/assets/icons/walletconnect.png" />
+          <div
+            class="
+              cursor-pointer
+              flex-none
+              order-1
+              h-4
+              text-xs
+              leading-4
+              my-0
+              mx-2
+              static
+              text-white
+              not-italic
+              wallet_name
+            "
+          >
+            WalletConnect &nbsp;
+          </div>
         </div>
-        <div @click="$emit('connect', 'fortmatic')" class="wallet_box">
-          <img src="../../assets/icons/formatic.png" />
-          <div class="wallet_name" @click="$emit('connect', 'fortmatic')">Fortmatic</div>
+        <div
+          @click="$emit('connect', 'fortmatic')"
+          class="
+            wallet_box
+            rounded-xl
+            cursor-pointer
+            flex flex-row
+            items-center
+            justify-start
+            flex-none
+            order-none
+            h-8
+            text-xs
+            my-0
+            mx-2
+            py-2
+            pl-2
+            pr-4
+            static
+            top-0
+            left-0
+          "
+        >
+          <img src="@/assets/icons/formatic.png" />
+          <div
+            class="
+              cursor-pointer
+              flex-none
+              order-1
+              h-4
+              text-xs
+              leading-4
+              my-0
+              mx-2
+              static
+              text-white
+              not-italic
+              wallet_name
+            "
+            @click="$emit('connect', 'fortmatic')"
+          >
+            Fortmatic
+          </div>
         </div>
-        <div @click="$emit('connect', 'portis')" class="wallet_box">
-          <img src="../../assets/icons/portis.png" />
-          <div class="wallet_name">Portis</div>
+        <div
+          @click="$emit('connect', 'portis')"
+          class="
+            wallet_box
+            rounded-xl
+            cursor-pointer
+            flex flex-row
+            items-center
+            justify-start
+            flex-none
+            order-none
+            h-8
+            text-xs
+            my-0
+            mx-2
+            py-2
+            pl-2
+            pr-4
+            static
+            top-0
+            left-0
+          "
+        >
+          <img src="@/assets/icons/portis.png" />
+          <div
+            class="
+              cursor-pointer
+              flex-none
+              order-1
+              h-4
+              text-xs
+              leading-4
+              my-0
+              mx-2
+              static
+              text-white
+              not-italic
+              wallet_name
+            "
+          >
+            Portis
+          </div>
         </div>
       </div>
-      <div class="learn_more_box">
-        Not sure how connect wallet works?
-        <span class="t_learn_more"
-          >Learn More &nbsp;<img src="../../assets/icons/externalLink.svg"
-        /></span>
+      <div
+        class="
+          learn_more_box
+          flex-none
+          order-none
+          h-4
+          text-xs
+          leading-4
+          mt-1
+          py-3
+          mx-4
+          absolute
+          left-0
+          not-italic
+          w-auto
+        "
+      >
+        <router-link to="#">
+          Not sure how connect wallet works?
+          <span class="inline-flex text-white">
+            Learn More &nbsp;<img src="@/assets/icons/externalLink.svg" />
+          </span>
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 
-<script >
-
-
-
+<script>
 export default {
   name: "ConnectWallet",
 
   methods: {
     close() {
-      console.log("sadfasdf");
+      console.log("sadfasdf")
       this.$emit("close")
     },
     saveWalletType() {
       this.$emit("saveWalletType")
     },
- 
-    
   },
 }
 </script>
 <style>
 .modal-backdrop {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
   background-color: rgba(0, 0, 0, 0.3);
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 
 .wallet_gif {
-  position: absolute;
-  width: 150px;
-  height: 136px;
   right: -10%;
   top: -20%;
 }
 
 .wallet_name {
-  position: static;
-  width: fit-content;
-  height: 15px;
   left: 33.23px;
   top: 8.5px;
-  cursor: pointer;
-
-  font-family: Open Sauce Sans;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 12px;
-  line-height: 15px;
-
-  /* identical to box height */
-
-  color: #ffffff;
-
-  /* Inside Auto Layout */
-
-  flex: none;
-  order: 1;
-  flex-grow: 0;
-  margin: 0px 8px;
+  font-family: inherit;
 }
 .wallet_box {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  padding: 8px 16px 8px 8px;
-  justify-content: flex-start;
-  position: static;
-  width: fit-content;
-  height: 31.09px;
-  left: 0px;
-  top: 0px;
   background: linear-gradient(180deg, rgba(143, 143, 255, 0.2) 0%, rgba(143, 143, 255, 0) 100%);
   box-shadow: inset 0px 0px 4px rgba(143, 143, 255, 0.2);
-  border-radius: 12px;
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-  margin: 0px 8px;
-  font-size: 12px;
 }
 
 .modal {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 16px;
-  position: absolute;
   width: 340px;
-  height: 239px;
-  left: 590px;
-  top: 221px;
-
-  background: #252562;
+  left: 50vw;
+  top: 50vh;
+  transform: translate(-50%, -50%);
+  background-color: rgba(37, 37, 98, 1);
   box-shadow: 0px 32px 64px -8px rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
 }
 
 .connect_wallet_header {
-  font-family: Open Sauce Sans;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 20px;
-  /* identical to box height */
-
-  color: #ffffff;
-
-  /* Inside Auto Layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-  margin: 8px 0px;
-}
-.modal-footer {
-  padding: 15px;
-  display: flex;
-}
-
-.t_learn_more {
-  display: inline-flex;
-  color: #ffffff;
+  font-family: inherit;
 }
 
 .learn_more_box {
-  position: absolute;
-  width: 236px;
-  height: 15px;
-  left: 0px;
   bottom: 20px;
-
-  font-family: Open Sauce Sans;
-  font-style: normal;
+  font-family: inherit;
   font-weight: normal;
-  font-size: 12px;
-  line-height: 15px;
-  /* identical to box height */
-  width: auto;
-  color: #cbcbff;
-
-  /* Inside Auto Layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-  margin: 0px 15px;
+  color: rgba(203, 203, 255, 1);
 }
 
 .connect_wallet_info {
-  position: static;
-  width: 215px;
-  height: 44px;
   left: 16px;
   top: 44px;
-
-  font-family: Open Sauce Sans;
-  font-style: normal;
+  font-family: inherit;
   font-weight: normal;
-  font-size: 14px;
   line-height: 160%;
-  /* or 22px */
-
-  color: #cbcbff;
-
-  /* Inside Auto Layout */
-
-  flex: none;
-  order: 1;
-  flex-grow: 0;
-  margin: 8px 0px;
+  color: rgba(203, 203, 255, 1);
 }
 
 .connect_wallet_container {
-  display: flex;
-  align-items: flex-start;
-  flex-wrap: wrap;
-  padding: 11px 10px;
-  position: absolute;
-  width: 308px;
-  height: 96px;
+  width: 310px;
   left: 16px;
   top: 96px;
-
-  background: #1d1d52;
+  background-color: rgba(29, 29, 82, 1);
   box-shadow: inset 2px 2px 4px rgba(0, 0, 0, 0.1);
-  border-radius: 16px;
 }
 .title {
-  position: relative;
-  color: #ffffff;
-  justify-content: space-between;
-  font-family: Open Sauce Sans;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 20px;
-}
-
-.modal-footer {
-  border-top: 1px solid #eeeeee;
-  flex-direction: column;
-  justify-content: flex-end;
-}
-
-.modal-body {
-  position: relative;
-  padding: 20px 10px;
-}
-
-.btn-close {
-  position: absolute;
-  top: 0;
-  right: 0;
-  border: none;
-  font-size: 20px;
-  padding: 10px;
-  cursor: pointer;
-  font-weight: bold;
-  color: #4aae9b;
-  background: transparent;
-}
-
-.btn-green {
-  color: white;
-  background: #4aae9b;
-  border: 1px solid #4aae9b;
-  border-radius: 2px;
+  font-family: inherit;
 }
 </style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,220 +1,277 @@
 <script>
-import  SynthsRoundedButton from "@/components/buttons/SynthsRoundedButton.vue";
+import SynthsRoundedButton from "@/components/buttons/SynthsRoundedButton.vue"
 
-    let tabs = [
-        {
-            id: 1,
-            title: "Explore Synths",
-            to: "explore-synths"
-        },
-        {
-            id: 2,
-            title: "Markets",
-            to: "markets"
-        },
-        {
-            id: 3,
-            title: "Portfolio",
-            to: "portfolio"
-        },
-    ];
-  
-    let activeTab = 0;
-    export default {
-        name: "Header",
-      components: {
-        's-button': SynthsRoundedButton
-      },
-        data() {
-            return {
-                tabs,
-              
-                activeTab,
-              
-            };
-        },
-        methods: {
-            selectTab(item) {
-                this.activeTab = item.id;
-            },
-          closePopup(e) {
-              e.stopPropagation()
-            this.isDropDownOpen = false
-            this.isWalletDropDownOpen = false;
-           // this.isModalVisible = false;
-          },
-        },
-  }
+let tabs = [
+  {
+    id: 1,
+    title: "Explore Synths",
+    to: "explore-synths",
+  },
+  {
+    id: 2,
+    title: "Markets",
+    to: "markets",
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    to: "portfolio",
+  },
+]
+
+let activeTab = 0
+export default {
+  name: "Header",
+  components: {
+    "s-button": SynthsRoundedButton,
+  },
+  data() {
+    return {
+      tabs,
+      activeTab,
+    }
+  },
+  methods: {
+    selectTab(item) {
+      this.activeTab = item.id
+    },
+    closePopup(e) {
+      e.stopPropagation()
+      this.isDropDownOpen = false
+      this.isWalletDropDownOpen = false
+      // this.isModalVisible = false;
+    },
+  },
+}
 </script>
 <script setup>
-import { useWeb3 } from '@/composables/useWeb3';
+import { useWeb3 } from "@/composables/useWeb3"
 import connectWalletModal from "../connectWallet/ConnectWallet.vue"
-import { ref } from  'vue';
+import { ref } from "vue"
 
-const { login, web3 ,logout} = useWeb3();
-const isModalVisible = ref(false);
+const { login, web3, logout } = useWeb3()
+const isModalVisible = ref(false)
 
-const isWalletDropDownOpen = ref(false);
-const isDropDownOpen = ref(false);
+const isWalletDropDownOpen = ref(false)
+const isDropDownOpen = ref(false)
 
-  async function handleConnect(connector) {
-    isModalVisible.value = false;
-    await login(connector);  
-  }
-  async function handleLogout() {
-    await logout(); 
-    // emit('close');
-  }
-  function formatAddress(address) {
-    return address.slice(0, 6) + '...' + address.slice(-6);
-  }
+async function handleConnect(connector) {
+  isModalVisible.value = false
+  await login(connector)
+}
+async function handleLogout() {
+  await logout()
+  // emit('close');
+}
+function formatAddress(address) {
+  return address.slice(0, 6) + "..." + address.slice(-6)
+}
 </script>
 
-
-
 <template>
-    <nav
-        class="h-12 sticky overflow-hidden top-0 left-0 z-20 bg-navy-blue-800 text-white flex flex-col lg:flex-row border-b bd-main bg-main"
-    >
-        <router-link to="/">
-        <div class="w-64 h-12 flex md:border-r bd-main">
-            <img src="@/assets/images/yamIcon.png" class="m-2 h-8 w-8">
-            <h3 class="logo font-bold text-emerald-500 my-auto">Yam Synths</h3>
-            <span class="bg-[#3468FF] my-auto px-2 overflow-hidden ml-3 md:ml-16 rounded-full text-sm">v 3.0</span>
-        </div>
-        </router-link>
-        <ul
-            class="flex space-x-8 overflow-hidden sticky left-64 my-auto px-4 rounded-full text-sm mx-4 py-1 font-semibold bg-dark txt-main invisible md:visible"
+  <nav
+    class="
+      h-12
+      sticky
+      overflow-hidden
+      top-0
+      left-0
+      z-20
+      bg-navy-blue-800
+      text-white
+      flex flex-col
+      lg:flex-row
+      border-b
+      bd-main
+      bg-main
+    "
+  >
+    <router-link to="/">
+      <div class="w-64 h-12 flex md:border-r bd-main">
+        <img src="@/assets/images/yamIcon.png" class="m-2 h-8 w-8" />
+        <h3 class="logo font-bold text-emerald-500 my-auto">Yam Synths</h3>
+        <span class="bg-[#3468FF] my-auto px-2 overflow-hidden ml-3 md:ml-16 rounded-full text-sm"
+          >v 3.0</span
         >
-            <li
-                class="min-w-max cursor-pointer px-2"
-                v-for="(tab, key) in tabs"
-                :key="key"
-                :class="tab.title==$route.name? 'bg-white rounded-md': ''"
+      </div>
+    </router-link>
+    <ul
+      class="
+        flex
+        space-x-8
+        overflow-hidden
+        sticky
+        left-64
+        my-auto
+        px-4
+        rounded-full
+        text-sm
+        mx-4
+        py-1
+        font-semibold
+        bg-dark
+        txt-main
+        invisible
+        md:visible
+      "
+    >
+      <li
+        class="min-w-max cursor-pointer px-2"
+        v-for="(tab, key) in tabs"
+        :key="key"
+        :class="tab.title == $route.name ? 'bg-white rounded-md' : ''"
+      >
+        <router-link :to="'/' + tab.to">
+          <span v-if="tab.title == $route.name" class="text-black">{{ tab.title }}</span>
+          <span v-else>{{ tab.title }}</span>
+        </router-link>
+      </li>
+    </ul>
+    <div
+      class="
+        flex
+        absolute
+        w-96
+        right-0
+        p-2
+        mr-4
+        text-right
+        h-12
+        lg:border-l
+        bd-main
+        invisible
+        md:visible
+      "
+    >
+      <div class="flex absolute right-1">
+        <img src="@/assets/images/bell.png" class="cursor-pointer my-auto h-4" />
+        <span
+          class="flex px-4 py-1.5 text-sm cursor-pointer"
+          @click="isDropDownOpen = !isDropDownOpen"
+        >
+          Help
+          <img src="@/assets/images/dropdown.svg" class="mx-2 my-auto h-4" />
+        </span>
+        <s-button
+          v-if="!$auth.isAuthenticated.value"
+          @click="isModalVisible = true"
+          buttonStyles="wallet-btn px-4 py-1.5 my-auto text-sm font-normal"
+        >
+          <template #buttonTitle> Connect Wallet </template>
+        </s-button>
+        <div>
+          <template v-if="$auth.isAuthenticated.value">
+            <span
+              v-if="$auth.isAuthenticated.value"
+              class="flex px-4 py-1.5 text-sm cursor-pointer"
+              @click=";(isWalletDropDownOpen = !isWalletDropDownOpen), (isDropDownOpen = false)"
             >
-                <router-link :to='"/" + tab.to'>
-                    <span v-if="tab.title==$route.name" class="text-black">{{ tab.title }}</span>
-                    <span v-else>{{ tab.title }}</span>
-                </router-link>
-            </li>
-        </ul>
-        <div class="flex absolute w-96 right-0 p-2 mr-4 text-right h-12 lg:border-l bd-main invisible md:visible">
-            <div class="flex absolute right-1">
-                <img src="@/assets/images/bell.png" class="cursor-pointer my-auto h-4">
-                <span
-                        class="flex px-4 py-1.5 text-sm cursor-pointer"
-                        @click="isDropDownOpen = !isDropDownOpen"
-                >
-                    Help
-                    <img src="@/assets/images/dropdown.svg" class="mx-2 my-auto h-4">
-                </span>
-              <s-button v-if="!$auth.isAuthenticated.value" @click="isModalVisible = true" buttonStyles="wallet-btn px-4 py-1.5 my-auto text-sm font-normal">
-                <template #buttonTitle>
-                  Connect Wallet
-                </template>
-              </s-button>
-	       <div>
-                <template v-if="$auth.isAuthenticated.value">
-                    <span
-                        v-if="$auth.isAuthenticated.value"
-                        class="flex px-4 py-1.5 text-sm cursor-pointer"
-                        @click=";(isWalletDropDownOpen = !isWalletDropDownOpen), (isDropDownOpen=false)"
-                    >
-                        <img src="../../assets/icons/metamask.svg" class="mx-2 my-auto h-4" />
-                        {{formatAddress(web3.account)}}
-                        <img src="../../assets/images/dropdown.svg" class="mx-2 my-auto h-4" />
-                    </span>
-                </template>
-            </div>
-            </div>
-            <ul
-                class="overflow-hidden my-auto p-2 text-sm text-left fixed top-9 right-44 bg-light rounded-xl"
-                v-if="isDropDownOpen"
-                v-click-away="closePopup"
-            >
-                <li class="min-w-max cursor-pointer p-1">
-                    <span>Documentation</span>
-                </li>
-                <li class="min-w-max cursor-pointer p-1">
-                    <span>Tutorials</span>
-                </li>
-            </ul>
-            <!-- wallet info dropdown -->
-            <ul
-                class="
-                overflow-hidden
-                my-auto
-                w-64
-                p-2
-                text-sm text-left
-                fixed
-                top-9
-                right-2
-                bg-light
-                rounded-xl
-                "
-                v-if="isWalletDropDownOpen"
-                 v-click-away="closePopup"
-            >
-                <li class="min-w-max cursor-pointer p-1">
-                <span class="t_network">Network</span>
-                </li>
-                <li class="min-w-max cursor-pointer p-1">
-                <label class="container"
-                    >Mainnet
-                    <input type="checkbox" :checked="web3.network.key==1" />
-                    <span class="checkmark"></span>
-                </label>
-                </li>
-                <li class="min-w-max cursor-pointer p-1">
-                <label class="container"
-                    >Polygon
-                    <input type="checkbox" :checked="web3.network.key==137" />
-                    <span class="checkmark"></span>
-                </label>
-                </li>
-                <li class="min-w-max cursor-pointer p-1">
-                <label class="container"
-                    >Rinkeby
-                    <input type="checkbox" :checked="web3.network.key==4" />
-                    <span class="checkmark"></span>
-                </label>
-                </li>
-                <li class="divider_dropdown_wallet"></li>
-                <li class="min-w-max cursor-pointer p-1">
-                <span class="wallet_actions"
-                    ><img src="../../assets/icons/copy.svg" /> &nbsp; Copy Address</span
-                >
-                </li>
-                <li class="min-w-max cursor-pointer p-1">
-                <span class="wallet_actions"
-                    ><img class="image_icon" src="../../assets/icons/externalLink.svg" />&nbsp;
-                    <a :href="web3.etherscanlink" >Etherscan</a></span
-                >
-                </li>
-                <li
-                @click=";handleLogout(), (isWalletDropDownOpen = false)"
-                class="min-w-max cursor-pointer p-1"
-                >
-                <span class="wallet_actions"
-                    ><img src="../../assets/icons/disconnect.svg" />&nbsp; Disconnect</span
-                >
-                </li>
-            </ul>
+              <img src="../../assets/icons/metamask.svg" class="mx-2 my-auto h-4" />
+              {{ formatAddress(web3.account) }}
+              <img src="../../assets/images/dropdown.svg" class="mx-2 my-auto h-4" />
+            </span>
+          </template>
         </div>
-        <div class="flex overflow-hidden absolute right-0 h-12 visible md:invisible">
-            <div class="flex px-4 py-4 cursor-pointer">
-                <img src="@/assets/images/green-dot.svg" class="h-full py-0.5">
-                <span class="text-xs my-auto font-normal px-1">13224549</span>
-            </div>
-        </div>
-        <connectWalletModal  v-show="isModalVisible" @close="closeModal" @connect="handleConnect" >
-  </connectWalletModal>
-    </nav>
-
+      </div>
+      <ul
+        class="
+          overflow-hidden
+          my-auto
+          p-2
+          text-sm text-left
+          fixed
+          top-9
+          right-44
+          bg-light
+          rounded-xl
+        "
+        v-if="isDropDownOpen"
+        v-click-away="closePopup"
+      >
+        <li class="min-w-max cursor-pointer p-1">
+          <span>Documentation</span>
+        </li>
+        <li class="min-w-max cursor-pointer p-1">
+          <span>Tutorials</span>
+        </li>
+      </ul>
+      <!-- wallet info dropdown -->
+      <ul
+        class="
+          overflow-hidden
+          my-auto
+          w-64
+          p-2
+          text-sm text-left
+          fixed
+          top-9
+          right-2
+          bg-light
+          rounded-xl
+        "
+        v-if="isWalletDropDownOpen"
+        v-click-away="closePopup"
+      >
+        <li class="min-w-max cursor-pointer p-1">
+          <span class="t_network">Network</span>
+        </li>
+        <li class="min-w-max cursor-pointer p-1">
+          <label class="container"
+            >Mainnet
+            <input type="checkbox" :checked="web3.network.key == 1" />
+            <span class="checkmark"></span>
+          </label>
+        </li>
+        <li class="min-w-max cursor-pointer p-1">
+          <label class="container"
+            >Polygon
+            <input type="checkbox" :checked="web3.network.key == 137" />
+            <span class="checkmark"></span>
+          </label>
+        </li>
+        <li class="min-w-max cursor-pointer p-1">
+          <label class="container"
+            >Rinkeby
+            <input type="checkbox" :checked="web3.network.key == 4" />
+            <span class="checkmark"></span>
+          </label>
+        </li>
+        <li class="divider_dropdown_wallet"></li>
+        <li class="min-w-max cursor-pointer p-1">
+          <span class="wallet_actions"
+            ><img src="../../assets/icons/copy.svg" /> &nbsp; Copy Address</span
+          >
+        </li>
+        <li class="min-w-max cursor-pointer p-1">
+          <span class="wallet_actions"
+            ><img class="image_icon" src="../../assets/icons/externalLink.svg" />&nbsp;
+            <a :href="web3.etherscanlink">Etherscan</a></span
+          >
+        </li>
+        <li
+          @click="handleLogout(), (isWalletDropDownOpen = false)"
+          class="min-w-max cursor-pointer p-1"
+        >
+          <span class="wallet_actions"
+            ><img src="../../assets/icons/disconnect.svg" />&nbsp; Disconnect</span
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="flex overflow-hidden absolute right-0 h-12 visible md:invisible">
+      <div class="flex px-4 py-4 cursor-pointer">
+        <img src="@/assets/images/green-dot.svg" class="h-full py-0.5" />
+        <span class="text-xs my-auto font-normal px-1">13224549</span>
+      </div>
+    </div>
+    <connectWalletModal
+      v-show="isModalVisible"
+      @close="isModalVisible = false"
+      @connect="handleConnect"
+    >
+    </connectWalletModal>
+  </nav>
 </template>
-
-
 
 <style scoped>
 .image_icon {
