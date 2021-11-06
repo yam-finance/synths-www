b96635e1a9c9ecc245a1d5a001d866a6533a1f64
wangtiger317 <hkc317@outlook.com>
2021-11-06 09:05:22 +0800

    conflict

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="home bg-main">
-    <Header />
-    <main>
-      <div class="w-full content">
-        <div>
-          <SideBar />
-        </div>
-        <div class="hidden md:flex content-desktop">
-          <router-view />
-        </div>
-        <div class="block md:hidden content-mobile">
-          <router-view />
-        </div>
-      </div>
-    </main>
-    <Footer />
-  </div>
+    <div class="home bg-main">
+        <Header />
+        <main>
+            <div class="w-full content">
+                <div>
+                    <SideBar />
+                </div>
+                <div class="hidden md:flex content-desktop">
+                    <router-view />
+                </div>
+                <div class="block md:hidden content-mobile">
+                    <router-view />
+                </div>
+            </div>
+        </main>
+        <Footer />
+    </div>
 </template>
 
 <script lang="ts">
@@ -24,24 +24,24 @@ import Header from "@/components/layout/Header.vue"
 import Footer from "@/components/layout/Footer.vue"
 
 export default {
-  name: "Home",
-  components: {
-    SideBar,
-    Header,
-    Footer,
-  },
+    name: "Home",
+    components: {
+        SideBar,
+        Header,
+        Footer,
+    },
 }
 </script>
 <style lang="scss">
 .content {
-  display: -webkit-inline-box;
+    display: -webkit-inline-box;
 
-  &-desktop {
-    width: calc(100vw - 256px);
-  }
+    &-desktop {
+        width: calc(100vw - 256px);
+    }
 
-  &-mobile {
-    height: calc(100vh - 64px);
-  }
+    &-mobile {
+        height: calc(100vh - 64px);
+    }
 }
 </style>
