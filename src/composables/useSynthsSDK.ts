0bffa6d993ee739fb517386f54df208cbf422f29
byterose <byterose@tutanota.com>
2021-11-16 17:48:01 +0100

    Update homepage

--- src/composables/useSynthsSDK.ts
+++ src/composables/useSynthsSDK.ts
@@ -1,25 +1,24 @@
 import { ref, computed } from "vue"
-import Synths, {getTotalMarketData, getRecentSynthData} from "synths-sdk"
-import {Web3Provider} from "@ethersproject/providers";
+import Synths, { getTotalMarketData, getRecentSynthData } from "synths-sdk"
+import { Web3Provider } from "@ethersproject/providers"
 
 const loading = ref(true)
 const data = ref({})
 const totalMarketData = ref()
 const recentSynthData = ref()
 const lspPortfolio = ref()
-let synthsSDK: Synths;
+const synthsSDK = ref()
 
 export function useSynthsSDK() {
-
     /**
      * @notice Initialize the synths-sdk and load market data.
      * @param provider The web3 provider instance.
      */
     async function init(provider: Web3Provider) {
-        const networkId: number = (await provider.getNetwork()).chainId;
-        synthsSDK = await Synths.create({ ethersProvider: provider });
-        recentSynthData.value = await getRecentSynthData(networkId);
-        totalMarketData.value = await getTotalMarketData([networkId]);
+        const networkId: number = (await provider.getNetwork()).chainId
+        // synthsSDK.value = await Synths.create({ ethersProvider: provider });
+        // recentSynthData.value = await getRecentSynthData(networkId, synthsSDK.value.assets)
+        // totalMarketData.value = await getTotalMarketData([networkId], synthsSDK.value.assets)
         // lspPortfolio.value = await getLSPPortfolio(provider.getSigner()._address);
     }
 
@@ -31,12 +30,10 @@ export function useSynthsSDK() {
     async function connectTo(param: string) {
         loading.value = true
 
-        const asset = await synthsSDK.connectAsset(param);
-        const empState = await asset.getEmpState();
+        const asset = synthsSDK.value.connectAsset(param)
+        const empState = await asset.getEmpState()
 
-        data.value[param] = {
-            "empState": empState
-        };
+        data.value[param] = { "empState": empState }
 
         loading.value = false
     }
@@ -47,8 +44,8 @@ export function useSynthsSDK() {
         totalMarketData: computed(() => totalMarketData.value),
         recentSynthData: computed(() => recentSynthData.value),
         lspPortfolio: computed(() => lspPortfolio.value),
-        synthsSDK,
+        synthsSDK: computed(() => synthsSDK.value),
         connectTo,
-        init
+        init,
     }
 }
