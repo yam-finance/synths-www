1ef785a023e131c55bfa670c2a3a77f771322699
wangtiger317 <hkc317@outlook.com>
2021-12-08 15:00:51 +0800

    fix top information boxes and missing line

--- src/views/ExploreSynths.vue
+++ src/views/ExploreSynths.vue
@@ -9,7 +9,7 @@
                 grid
                 md:grid-cols-1
                 lg:grid-cols-1
-                xl:grid-cols-2
+                xl:grid-cols-3
                 2xl:grid-cols-3
                 gap-4
                 text-white
